f1b6f6024259d637e56d041f60c310b2ef06b35d
geronde <geronde@docker.home>
2020-08-12 02:03:51 +0100

    add header and footer

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,5 +2,6 @@ class HomePageController < ApplicationController
   def index
     allPokemons = Pokemon.all
     @pokemons = allPokemons.map {|poke| {name: poke["name"]}}
+    @title = 'Pokemons'
   end
 end
