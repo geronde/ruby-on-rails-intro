40e149a3221ad94cd544cb8ecfdec4485ffad898
geronde <geronde@docker.home>
2020-08-13 01:34:34 +0100

    refacto + add pokemon view

--- app/controllers/home_page_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomePageController < ApplicationController
-  def index
-    allPokemons = Pokemon.all
-    @pokemons = allPokemons.map {|poke| {name: poke["name"]}}
-    @title = 'Pokemons'
-  end
-end
