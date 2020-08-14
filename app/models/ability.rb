a19565b0f253be1c3c631fd32757811af12f0b49
geronde <geronde@docker.home>
2020-08-14 18:41:32 +0100

    add linter and rubycop all files

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Ability < ApplicationRecord
-    has_and_belongs_to_many :pokemons, :join_table => "abilities_pokemons"
+  has_and_belongs_to_many :pokemons, join_table: 'abilities_pokemons'
 end
